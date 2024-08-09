e742b4b6c658976087f12ab7b6fbfc323168bc70
balankdharan <balankdharan@gmail.com>
2024-08-09 22:01:52 +0530

    changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,13 @@
 export const signup = async (req, res) => {
   try {
-    res.send("signup");
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   } catch (err) {}
 };
 export async function login(req, res) {
