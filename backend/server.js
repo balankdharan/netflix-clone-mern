eecb689199b2c77d838b12abc1fa2431b414dfc2
balankdharan <balankdharan@gmail.com>
2024-08-11 21:24:26 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 
+dotenv.config();
 const app = express();
 
 app.use("/api/v1/auth", authRoutes);
