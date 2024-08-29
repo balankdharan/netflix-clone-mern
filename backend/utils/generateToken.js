7ff586a59a02b03e1d409a07b1f7458c4333506f
balankdharan <balankdharan@gmail.com>
2024-08-29 22:54:32 +0530

    changes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,6 +7,6 @@ export const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    // secure:
+    secure: ENV_VARS.NODE_ENV !== "development",
   });
 };
