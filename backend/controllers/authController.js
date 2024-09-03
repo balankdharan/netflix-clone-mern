00fec2193da5ed64d2b9277f6d0acf15ab0d57e7
balankdharan <balankdharan@gmail.com>
2024-09-03 22:26:41 +0530

    changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -75,7 +75,8 @@ export async function login(req, res) {
 }
 export async function logout(req, res) {
   try {
-    res.send("logout");
+    res.clearCookie("jwt-netflix");
+    res.status(200).json({ success: true, message: "Successfully logged out" });
   } catch (err) {
     console.log("err", err);
     return res.status(500).json({
