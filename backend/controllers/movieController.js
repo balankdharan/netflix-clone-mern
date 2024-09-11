30e853a3f2c49bf36993324395a3e98085f93d92
balankdharan <balankdharan@gmail.com>
2024-09-11 20:38:26 +0530

    changes

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -2,6 +2,7 @@ import { fetchFromTMDB } from "../services/tmdb.service.js";
 export async function getTrendingMovie(req, res) {
   try {
     const data = await fetchFromTMDB("url");
+    const randomMovie = data.results[Math.round() * data.results?.length];
     res.status(200).json({ success: true, message: "Successfully logged out" });
   } catch (err) {
     console.log("err", err);
