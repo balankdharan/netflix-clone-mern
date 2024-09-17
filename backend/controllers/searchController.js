6c8adb6f76ac59b5d5e682566a83928cc8e1f6f9
balankdharan <balankdharan@gmail.com>
2024-09-17 08:34:05 +0530

    changes

---
+++ backend/controllers/searchController.js
@@ -0,0 +1,60 @@
+import { User } from "../models/user.js";
+
+export async function searchPerson(req, res) {
+  const { query } = req.params;
+  try {
+    const response = await fetchFromTMDB("url");
+
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].profile_path,
+          title: response.results[0].name,
+          searchType: "person",
+          createdAt: new Date(),
+        },
+      },
+    });
+    res.status(200).json({ success: true, content: response.results });
+  } catch (err) {
+    console.log("err", err);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+}
+export async function searchMovie(req, res) {
+  const { query } = req.params;
+  try {
+    const response = await fetchFromTMDB("url");
+    res.status(200).json({ success: true, content: response.results });
+  } catch (err) {
+    console.log("err", err);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+}
+export async function searchTv(req, res) {
+  const { query } = req.params;
+  try {
+    const data = await fetchFromTMDB("url");
+    res.status(200).json({ success: true, content: data.results });
+  } catch (err) {
+    console.log("err", err);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+}
