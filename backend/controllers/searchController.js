928fc94a2e24b9caa35a7aec12fc2acf79f336d1
balankdharan <balankdharan@gmail.com>
2024-09-24 17:48:25 +0530

    changes

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -104,8 +104,19 @@ export async function getSearchHistory(req, res) {
   }
 }
 export async function removeItemFromHistory(req, res) {
-  const { id } = req.params;
+  let { id } = req.params;
+  id = parseInt(id);
   try {
+    await User.findByIdAndUpdate(req.user._id, {
+      $pull: {
+        searchHistory: {
+          id: id,
+        },
+      },
+    });
+    res
+      .status(200)
+      .json({ success: true, message: "Item removed from search history" });
   } catch (err) {
     console.log("err", err);
 
