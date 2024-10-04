e34dede4f525aa7c06509ec8df7dca8ccc1c2e3d
balankdharan <balankdharan@gmail.com>
2024-10-04 23:12:14 +0530

    API tested

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
-  return <div>Login page</div>;
+  return (
+    <div className="h-screen w-full hero-bg">
+      <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
+        <Link to={"/"}>
+          <img src="/netflix-logo.png" alt="log" />
+        </Link>
+      </header>
+    </div>
+  );
 };
 
 export default LoginPage;
