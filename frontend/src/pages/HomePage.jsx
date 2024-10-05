99c7a1bbb76f634cdd8b554ec0d6be0d9aadb962
balankdharan <balankdharan@gmail.com>
2024-10-05 22:22:57 +0530

    API tested

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <div className="hero-bg h-screen">HomePage</div>;
+  return (
+    <div className="hero-bg h-screen">
+      <div className="h-screen w-full hero-bg">
+        <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
+          <Link to={"/"}>
+            <img src="/netflix-logo.png" alt="log" />
+          </Link>
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
