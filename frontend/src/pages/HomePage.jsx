f9a3572f8aaa3a8a91e79b0fd8dd3c0b6df7e1d2
balankdharan <balankdharan@gmail.com>
2024-10-06 21:47:19 +0530

    API tested

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,9 +5,7 @@ const HomePage = () => {
     <div className="hero-bg h-screen">
       <div className="h-screen w-full hero-bg">
         <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
-          <Link to={"/"}>
-            <img src="/netflix-logo.png" alt="log" />
-          </Link>
+          <Link to={"/"}></Link>
         </header>
       </div>
     </div>
