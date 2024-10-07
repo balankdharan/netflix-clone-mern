73dd35036683c6165c3ed9455569cb9aeb9e1296
balankdharan <balankdharan@gmail.com>
2024-10-07 20:33:10 +0530

    API tested

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -8,6 +8,14 @@ const SignUp = () => {
           <img src="/netflix-logo.png" alt="log" />
         </Link>
       </header>
+
+      <div className="flex justify-center items-center mt-20 mx-3">
+        <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
+          <h1 className="text-center text-white text-2xl font-bold mb-4">
+            Sign up
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
