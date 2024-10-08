fb411345f91cc2dc25211e9f00b079e24827e0d3
balankdharan <balankdharan@gmail.com>
2024-10-08 22:15:06 +0530

    API tested

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -8,6 +8,13 @@ const LoginPage = () => {
           <img src="/netflix-logo.png" alt="log" />
         </Link>
       </header>
+      <div className="flex justify-center items-center mt-20 mx-3">
+        <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
+          <h1 className="text-center text-white text-2xl font-bold mb-4">
+            Login
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
