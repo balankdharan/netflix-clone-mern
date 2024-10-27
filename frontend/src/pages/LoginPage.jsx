a571181555b366760b3ddcf0bbcb412db9a9e098
balankdharan <balankdharan@gmail.com>
2024-10-27 20:09:23 +0530

    Previous works Updates

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -13,7 +13,36 @@ const LoginPage = () => {
           <h1 className="text-center text-white text-2xl font-bold mb-4">
             Login
           </h1>
-          <form className="space-y-4"></form>
+          <form className="space-y-4">
+            <div>
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+                placeholder="you@example.com"
+                id="email"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="username"
+                className="text-sm font-medium text-gray-300 block"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+                placeholder="johndoe"
+                id="username"
+              />
+            </div>
+          </form>
         </div>
       </div>
     </div>
