05b52cfce5d90676294ef737cde26436d4c2b6b6
balankdharan <balankdharan@gmail.com>
2024-11-06 21:15:53 +0530

    udpates

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("data", email, password);
+  };
   return (
     <div className="h-screen w-full hero-bg">
       <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
@@ -12,12 +15,13 @@ const LoginPage = () => {
           <img src="/netflix-logo.png" alt="log" />
         </Link>
       </header>
+
       <div className="flex justify-center items-center mt-20 mx-3">
         <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
           <h1 className="text-center text-white text-2xl font-bold mb-4">
             Login
           </h1>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleLogin}>
             <div>
               <label
                 htmlFor="email"
@@ -30,22 +34,11 @@ const LoginPage = () => {
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
                 placeholder="you@example.com"
                 id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
-            <div>
-              <label
-                htmlFor="username"
-                className="text-sm font-medium text-gray-300 block"
-              >
-                Username
-              </label>
-              <input
-                type="text"
-                className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
-                placeholder="johndoe"
-                id="username"
-              />
-            </div>
+
             <div>
               <label
                 htmlFor="password"
@@ -58,18 +51,20 @@ const LoginPage = () => {
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
                 placeholder="******"
                 id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">
               Login
             </button>
           </form>
-        </div>
-        <div className="text-center text-gray-400">
-          Already a member?{" "}
-          <Link to={"/login"} className="text-red-500 hover:underline">
-            Sign In
-          </Link>
+          <div className="text-center text-gray-400">
+            Don&apos;t have an account?{" "}
+            <Link to={"/signup"} className="text-red-500 hover:underline">
+              Sign Up
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -5,6 +5,10 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log("data", email, username, password);
+  };
   return (
     <div className="h-screen w-full hero-bg">
       <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
@@ -18,7 +22,7 @@ const SignUp = () => {
           <h1 className="text-center text-white text-2xl font-bold mb-4">
             Sign up
           </h1>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSignUp}>
             <div>
               <label
                 htmlFor="email"
@@ -31,6 +35,8 @@ const SignUp = () => {
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
                 placeholder="you@example.com"
                 id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div>
@@ -45,6 +51,8 @@ const SignUp = () => {
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
                 placeholder="johndoe"
                 id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
               />
             </div>
             <div>
@@ -59,16 +67,20 @@ const SignUp = () => {
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
                 placeholder="******"
                 id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <button>Sign up</button>
+            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">
+              Sign up
+            </button>
           </form>
-        </div>
-        <div className="text-center text-gray-400">
-          Already a member?{" "}
-          <Link to={"/login"} className="text-red-500 hover:underline">
-            Sign In
-          </Link>
+          <div className="text-center text-gray-400">
+            Already a member?{" "}
+            <Link to={"/login"} className="text-red-500 hover:underline">
+              Sign In
+            </Link>
+          </div>
         </div>
       </div>
     </div>
