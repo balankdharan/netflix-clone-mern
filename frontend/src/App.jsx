58fd3e204b7cb2c1095c640302a9ebd6c90e4199
balankdharan <balankdharan@gmail.com>
2024-11-07 22:52:22 +0530

    udpates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUp from "./pages/SignUp";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
