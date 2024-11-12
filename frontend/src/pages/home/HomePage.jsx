6f72f1df24283616b388a9c005b4f5819608fe01
balankdharan <balankdharan@gmail.com>
2024-11-12 22:30:23 +0530

    udpates

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,14 +1,18 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import AuthScreen from "./AuthScreen";
+import HomeScreen from "./HomeScreen";
 
 const HomePage = () => {
+  const user = false;
   return (
-    <div className="hero-bg h-screen">
-      <div className="h-screen w-full hero-bg">
-        <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
-          <Link to={"/"}></Link>
-        </header>
-      </div>
-    </div>
+    // <div className="hero-bg h-screen">
+    //   <div className="h-screen w-full hero-bg">
+    //     <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
+    //       <Link to={"/"}></Link>
+    //     </header>
+    //   </div>
+    // </div>
+    <div>{user ? <HomeScreen /> : <AuthScreen />}</div>
   );
 };
 
