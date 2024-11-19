bbb3e0de5ddedc4d1cb3fa3ed471280f83e1b734
balankdharan <balankdharan@gmail.com>
2024-11-19 20:18:48 +0530

    udpates

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,27 +1,36 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+// import { useAuthStore } from "../store/authUser";
 
-const SignUp = () => {
-  const [email, setEmail] = useState("");
+const SignUpPage = () => {
+  const { searchParams } = new URL(document.location);
+  const emailValue = searchParams.get("email");
+
+  const [email, setEmail] = useState(emailValue || "");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  // const { signup, isSigningUp } = useAuthStore();
+
   const handleSignUp = (e) => {
     e.preventDefault();
-    console.log("data", email, username, password);
+    // signup({ email, username, password });
   };
+
   return (
     <div className="h-screen w-full hero-bg">
       <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
         <Link to={"/"}>
-          <img src="/netflix-logo.png" alt="log" />
+          <img src="/netflix-logo.png" alt="logo" className="w-52" />
         </Link>
       </header>
 
       <div className="flex justify-center items-center mt-20 mx-3">
         <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
           <h1 className="text-center text-white text-2xl font-bold mb-4">
-            Sign up
+            Sign Up
           </h1>
+
           <form className="space-y-4" onSubmit={handleSignUp}>
             <div>
               <label
@@ -39,6 +48,7 @@ const SignUp = () => {
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
+
             <div>
               <label
                 htmlFor="username"
@@ -55,6 +65,7 @@ const SignUp = () => {
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
+
             <div>
               <label
                 htmlFor="password"
@@ -65,20 +76,26 @@ const SignUp = () => {
               <input
                 type="password"
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
-                placeholder="******"
+                placeholder="••••••••"
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">
-              Sign up
-            </button>
+
+            {/* <button
+							className='w-full py-2 bg-red-600 text-white font-semibold rounded-md
+							hover:bg-red-700
+						'
+							disabled={isSigningUp}
+						>
+							{isSigningUp ? "Loading..." : "Sign Up"}
+						</button> */}
           </form>
           <div className="text-center text-gray-400">
             Already a member?{" "}
             <Link to={"/login"} className="text-red-500 hover:underline">
-              Sign In
+              Sign in
             </Link>
           </div>
         </div>
@@ -86,5 +103,4 @@ const SignUp = () => {
     </div>
   );
 };
-
-export default SignUp;
+export default SignUpPage;
