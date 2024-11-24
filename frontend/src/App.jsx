6f79363306d6cdf01eb7ac9a6aaa351029a5cab3
balankdharan <balankdharan@gmail.com>
2024-11-24 21:07:33 +0530

    udpates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUp from "./pages/SignUp";
 import HomePage from "./pages/home/HomePage";
 import Footer from "./components/Footer";
+import { useAuthStore } from "./store/authUser";
+import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
 
 function App() {
+  const { user, isCheckingAuth, authCheck } = useAuthStore();
+  useEffect(() => {
+    authCheck();
+  }, [authCheck]);
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/login"
+          element={!user ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <SignUp /> : <Navigate to={"/"} />}
+        />
       </Routes>
       <Footer />
     </>
