fe164e1cc10f30c594c5abfa98dbd57c622079f3
balankdharan <balankdharan@gmail.com>
2024-11-25 21:42:46 +0530

    updates

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/authController.js";
-
+import {
+  login,
+  logout,
+  signup,
+  authCheck,
+} from "../controllers/authController.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 const router = express.Router();
 
 router.get("/signup", signup);
 router.get("/login", login);
 router.get("/logout", logout);
+
+router.get("/authCheck", protectRoute, authCheck);
 
 export default router;
