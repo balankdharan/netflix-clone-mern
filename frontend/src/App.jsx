873d2573520c445b9fbe8e458ac2b21c0d4955a4
balankdharan <balankdharan@gmail.com>
2024-11-28 22:06:03 +0530

    translation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SignUp from "./pages/SignUp";
 import HomePage from "./pages/home/HomePage";
 import Footer from "./components/Footer";
 import { useAuthStore } from "./store/authUser";
+import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
 
@@ -12,6 +13,15 @@ function App() {
   useEffect(() => {
     authCheck();
   }, [authCheck]);
+  if (isCheckingAuth) {
+    return (
+      <div className="h-screen">
+        <div className="flex justify-center items-center bg-black h-full">
+          <Loader className="animate-spin text-red-600 size-10" />
+        </div>
+      </div>
+    );
+  }
   return (
     <>
       <Routes>
@@ -26,6 +36,7 @@ function App() {
         />
       </Routes>
       <Footer />
+      <Toaster />
     </>
   );
 }
